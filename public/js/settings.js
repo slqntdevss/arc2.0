3e2216bb089c3f8e7dae8ddbbb7ce3b8667b1681
insert <139027498+insertdevs@users.noreply.github.com>
2024-06-07 07:27:25 -0400

    about:blank fix maybe

--- public/js/settings.js
+++ public/js/settings.js
@@ -36,19 +36,22 @@ function resetData() {
     window.top.location.reload();
   }
 }
-  function ab() {
-    var ab = window.open(
-      "",
-      "myWindow1",
-      "scrollbars=1,height=" + screen.availHeight + ",width=" + screen.availWidth,
-    );
-    ab.document.write(
-      `<!DOCTYPE html>\n\
-  <title>Google</title>\n\
-  <iframe src=${window.top.location.host} frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%">\n\
-  <script>\n\
-  alert("");\n\
-  <\x2fscript>`,
-    );
-    window.top.location.replace("https://classroom.google.com");
-  }
+function ab() {
+  var myWindow1 = window.open(
+    "",
+    "myWindow1",
+    "scrollbars=1,height=" + screen.availHeight + ",width=" + screen.availWidth,
+  );
+  myWindow1.document.write(
+    '<!DOCTYPE html>\n\
+<title>Google</title>\n\
+<p><iframe src="' +
+      "https://" +
+      window.location.host +
+      '"frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%">\n\
+<script>\n\
+alert("");\n\
+<\x2fscript>',
+  );
+  location.replace("https://classroom.google.com");
+}
